f63b5f217facbb84d2ea4aa1e321c6eb2d669218
mreed5812 <mreed5812@gmail.com>
2019-03-12 22:08:13 -0500

    No changes

---
+++ Index.js
@@ -0,0 +1,43 @@
+
+
+function drawGrid(gridSide, gridTotal) {
+    for(i=0; i<gridTotal; i++){
+        div = document.createElement('div')
+        div.setAttribute('id', 'gridBox')
+        gridContainer.appendChild(div);
+    }
+    gridContainer.setAttribute('style', 'grid: repeat(' + gridSide + ',auto) / repeat(' + gridSide + ',auto)')
+}
+
+function blackOnHover() {
+    var divs = document.querySelectorAll('.grid-container div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', function(e) {
+            e.target.setAttribute('style', 'background: black');
+        });
+    });
+}
+
+function gameSetup() {
+	document.getElementById('reset').addEventListener('click', function(){
+		var divs = document.querySelectorAll('.grid-container div')
+    	divs.forEach((div) => {
+    		div.setAttribute('style', 'background: white');
+    	});
+	});
+}
+
+
+//setup
+const gridContainer = document.querySelector('.grid-container');
+const GridSide = 16;      // initial gridSide is 4, set the grid attribute to repeat 4x4
+gridContainer.setAttribute('style', 'grid: repeat(' + GridSide + ',auto) / repeat(' + GridSide + ',auto)');
+const GridTotal = GridSide * GridSide;  
+
+//create grid
+drawGrid(GridSide, GridTotal);
+
+blackOnHover();
+
+gameSetup();
+
---
+++ js/Index.js
@@ -0,0 +1,12 @@
+
+var div = document.createElement("div");
+
+
+//Append divs to main div
+for(i = 0; i < 16; i++){
+	document.getElementById("main").appendChild(div);
+}
+
+console.log("Test");
+
+
