9bcc21d158ae073898106d80787936d34a9c42fa
mreed5812 <mreed5812@gmail.com>
2019-03-24 18:24:47 -0500

    added random color function

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
     blackOnHover();
 });
 
-
-
-
 function drawGrid(gridSide, gridTotal) {
     for(i=0; i<gridTotal; i++){
         div = document.createElement('div')
@@ -25,6 +22,15 @@ function blackOnHover() {
     });
 }
 
+function rainbow() {
+    var divs = document.querySelectorAll('.grid-container div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', function(e) {
+            e.target.setAttribute('style', 'background:' + getRandomColor());
+        });
+    });
+}
+
 function erase(){
     var divs = document.querySelectorAll('.grid-container div');
     divs.forEach((div) => {
@@ -39,16 +45,37 @@ function gameSetup() {
     divs.forEach((div) => {
     	div.setAttribute('style', 'background: white');
     });
+    blackOnHover();
 }
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 
 var resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', function(){
         gameSetup();
-});
+    });
+
+var rainbowButton = document.getElementById('rainbow');
+    rainbowButton.addEventListener('click', function(){
+        rainbow();
+    });
 
 var eraserButton = document.getElementById('erase');
     eraserButton.addEventListener('click', function(){
         erase();
+    });
+
+var blackButton = document.getElementById('black');
+    blackButton.addEventListener('click', function(){
+        blackOnHover();
     });
 
 //setup
@@ -56,11 +83,3 @@ const gridContainer = document.querySelector('.grid-container');
 const GridSide = 16;      // initial gridSide is 4, set the grid attribute to repeat 4x4
 gridContainer.setAttribute('style', 'grid: repeat(' + GridSide + ',auto) / repeat(' + GridSide + ',auto)');
 const GridTotal = GridSide * GridSide;  
-
-//create grid
-//drawGrid(GridSide, GridTotal);
-
-//blackOnHover();
-
-//gameSetup();
-
